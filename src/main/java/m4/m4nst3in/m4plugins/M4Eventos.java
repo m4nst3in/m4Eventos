84df4cb0be5a37c504cc4a62f8f3eaa02fbaba06
m4nst3in <m4nsdev@proton.me>
2025-03-18 17:20:27 -0300

    initialize m4Eventos plugin with Gradle setup and basic structure

---
+++ src/main/java/m4/m4nst3in/m4plugins/M4Eventos.java
@@ -0,0 +1,17 @@
+package m4.m4nst3in.m4plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class M4Eventos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
